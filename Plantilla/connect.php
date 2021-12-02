0c5bb081396453b02b94955ed518ceaaca949913
ramon90 <ramon_ramirez90@hotmail.com>
2021-12-02 16:54:12 -0600

    intentando archivo de conect

---
+++ Plantilla/connect.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+<?php
+	$mysqli= new mysqli("localhost","root","","mydraftproject");
+	if(mysqli_connect_errno()){
+		echo "no se pudo conectar";
+	}
+fgdfgdfg
+?>
+<body>
+</body>
+</html>
