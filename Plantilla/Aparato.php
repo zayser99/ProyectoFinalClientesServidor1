00932cd947230369d1fb6cd93e99b4cd7d387be5
ramon90 <ramon_ramirez90@hotmail.com>
2021-12-03 23:29:48 -0600

    algo

--- Plantilla/Aparato.php
+++ Plantilla/Aparato.php
@@ -70,54 +70,9 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="section-heading">
-                        <div style="color:white;background:green;opacity:0.8;font-family: serif;font-weight: bolder;font-size: xx-large">
-						<br>CALCULADORA DE ENERGÍAS LIMPIAS
-                        <h1 >Elige el tipo de aparato:</h1>
-                        <h3 style="text-align:center;" >¿No encontraste tu aparato? Registra <a href="Aparato.php" style="color:white;">Aquí</a>     </h3><br>
-                        </div>
-						<center><div class="calculadora">
                         <div  style="padding-left: 10px; padding-right: 5px;opacity:0.9;" class="panel panel-default">
-							<div class="container">
-								<h1 style="font-family: serif;font-weight: bolder;">APARATOS</h1>
-								<div class="row">
-								<?php 
-								include('connect.php');
-								$query = "SELECT * FROM aparato";
-								$resultados = mysqli_query($mysqli,$query);
-								while($row = $resultados->fetch_assoc()){
-								?>
-									<div class="col-md-3 col-sm-6">
-										<div class="product-grid2">
-											<div class="product-image2">
-												<a href="#">
-												<img class="pic-1" width="100%" height="240" src="data:image/jpg;base64, <?php echo base64_encode($row['imagen_producto']); ?>">
-												<img class="pic-2" width="100%" height="240" src="data:image/jpg;base64, <?php echo base64_encode($row['imagen_producto']); ?>">
-												</a>
-											</div>
-											<div class="product-content">
-												<h3 class="title"><a href="#"> <?php echo $row['nombre_aparato'];  ?> </a></h3>
-												<span class="price"><?php echo $row['consumo_ind'];  ?> wh/h</span>
-                                                <form action="p_calculo.php" method="POST">
-											<input type="hidden" name="aparato" value="<?php echo $row['nombre_aparato'];?>">
-											<input type="hidden" name="consumo" value="<?php echo $row['consumo_ind'];?>">
-											
-											<input style="text-align:center" type="number"  min="1" max="50" step="1" name="cantidad" value="1" >
-											<br><br><br><br>
-											<button type="submit" name="agrega_calculo" class="add-to-cart">AGREGAR</button>
-											</form>
-                                            </div>
-											
-										</div>
-                                  	</div>
-                                    <?php
-									    }
-									    ?>
-								</div>
-							</div>
-                            </div>
-							<hr>							
-						</div></center><!--hasta aqui va un registro de aparato-->
-                    	
+                        
+                        </div>
 					</div>
 				</div>
 			</div>
